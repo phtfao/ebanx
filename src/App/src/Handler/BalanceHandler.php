3cbefb404768e6a2a4f713e2345bcd8c3903882c
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <paulo.fonseca@itamaraty.gov.br>
2021-01-16 05:00:14 -0300

    Adding logic to receive account_id and return its balance.

--- src/App/src/Handler/BalanceHandler.php
+++ src/App/src/Handler/BalanceHandler.php
@@ -7,11 +7,28 @@ namespace App\Handler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
 
 class BalanceHandler implements RequestHandlerInterface
 {
+    const BALANCE_ADDR = __DIR__ . "/../../../../data/";
+
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        // Create and return a response
+        $accountId = $request->getQueryParams()['account_id'] ?? '';
+        $accountId = (int)htmlspecialchars($accountId, ENT_HTML5, 'UTF-8');        
+
+        if (is_nan($accountId)) {
+            return new HtmlResponse(0, 404);
+            
+        }
+
+        $accountBalanceAddr = self::BALANCE_ADDR . "{$accountId}.balance";
+
+        if (! is_file($accountBalanceAddr) ) {
+            return new HtmlResponse('0', 404);
+        }
+        $balance = file_get_contents($accountBalanceAddr);
+        return new HtmlResponse($balance, 200);
     }
 }
