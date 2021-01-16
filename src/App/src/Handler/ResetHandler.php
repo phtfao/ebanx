1d576a1acace5f5f9d9219f7e88dd42cf44a7de2
Paulo Henrique Tameirão Fonseca Araújo de Oliveira <paulo.fonseca@itamaraty.gov.br>
2021-01-16 07:32:28 -0300

    Adding logic to reset the application data.

--- src/App/src/Handler/ResetHandler.php
+++ src/App/src/Handler/ResetHandler.php
@@ -7,11 +7,15 @@ namespace App\Handler;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
 
 class ResetHandler implements RequestHandlerInterface
 {
+    const BALANCE_ADDR = __DIR__ . "/../../../../data/";
+
     public function handle(ServerRequestInterface $request) : ResponseInterface
     {
-        // Create and return a response
+        array_map('unlink', glob(self::BALANCE_ADDR . "*.balance"));
+        return new HtmlResponse("OK");
     }
 }
